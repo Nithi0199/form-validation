39523141eae265496089bf524c78ec4e7622bdda
Nithi0199 <nithinbalan01@gmail.com>
2023-07-18 23:20:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+ const usernameInput=document.getElementById
+ ('username' );
+
+const usernameInput=document.getElementById 
+('email')
+
+const usernameInput=document.getElementById
+('password')
+
+const usernameInput=document.getElementById
+('confirmpassword')
+const btnsubmit = document.getElementById
+('btn-submit')
+const formE1=document.getElementById('form')
+
+   btn-Submit.addEventlistener('submit,function(Event')
+{
+    Event.preventDefault();
+    const username=usernameInput.value;
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    const comfirmpassword = passwordInput.value;
+
+    console.log('Username:',username);
+    console.log('Email:',Email);
+    console.log('Password:',Password);
+    console.log('Confirm Password:',comfirmpassword);
+    
+}
